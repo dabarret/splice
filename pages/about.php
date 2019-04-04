fe0fdf09076032332c3f36d12ce9d723cc5539be
dabarret <49283265+dabarret@users.noreply.github.com>
2019-04-04 12:46:18 -0400

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -16,8 +16,8 @@
 			</div>
 			<div class="intro-desc">
 				<img src="../img/masuda.jpg">
-				<p><span>What is S.P.L.I.C.E?</span><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Nunc sed blandit libero volutpat sed cras. Ullamcorper velit sed ullamcorper morbi. Ut etiam sit amet nisl purus in. Cursus in hac habitasse platea dictumst quisque. Vulputate eu scelerisque felis imperdiet proin. Enim tortor at auctor urna nunc. Tempor id eu nisl nunc. Vitae semper quis lectus nulla. Vel eros donec ac odio tempor orci dapibus ultrices. Ipsum faucibus vitae aliquet nec ullamcorper. Facilisi etiam dignissim diam quis enim.<br>
-				Diam phasellus vestibulum lorem sed. Nullam non nisi est sit amet facilisis magna etiam tempor. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Hendrerit dolor magna eget est lorem. Mattis enim ut tellus elementum sagittis vitae et leo. Ullamcorper a lacus vestibulum sed arcu non odio. Sapien eget mi proin sed libero enim sed faucibus. At lectus urna duis convallis convallis tellus. Faucibus turpis in eu mi bibendum neque egestas congue. Arcu cursus vitae congue mauris rhoncus.</p>
+				<p><span>Create, submit, edit and review your projects more effciently using S.P.L.I.C.E. Student Project Listing and Integrated Comment Environment (S.P.L.I.C.E.),<br>
+					is a system that allows Dr. Masuda's chemistry students to have a centralized workspace or discussion area for gene sequencing projects. </p>
 			</div>
 		</div>
 	</section>
